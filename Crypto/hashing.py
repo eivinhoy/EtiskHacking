ded72579a38b773c6240027dfd6edd59f43784f8
eivinhoy <eivind@hoydal.net>
2019-11-28 18:20:54 +0100

    added time on hashes

--- Crypto/hashing.py
+++ Crypto/hashing.py
@@ -1,11 +1,16 @@
 from Crypto.Hash import SHA256
 import hashlib
 import binascii
+import time
 
 
 # hashing with SHA256
 def hash(plaintext):
-    return SHA256.new(plaintext.encode()).hexdigest()
+    origin_time = time.time()
+    sha = SHA256.new(plaintext.encode()).hexdigest()
+    time_interval = time.time() - origin_time
+    print("mode - {0}".format(time_interval))
+    return sha
 
 
 print(hash("abc"))
@@ -19,7 +24,10 @@ print(check_password("abc", hash("abc")))
 
 # key derivation with salt 
 def derived_key_hash(plaintext, salt, iterations):
+    origin_time = time.time()
     derived_key = hashlib.pbkdf2_hmac('sha256', plaintext.encode(), salt.encode(), iterations)
+    time_interval = time.time() - origin_time
+    print("time - {0}".format(time_interval))
     return binascii.hexlify(derived_key)
 
-print(derived_key_hash("abc", "salt", 100000))
+print(derived_key_hash("abc", "salt", 1000000))
